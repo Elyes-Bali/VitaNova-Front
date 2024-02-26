9536cce5bbec6d44b2baa2541c415f2fee3ccf64
Elyes Bali <baliaynos@gmail.com>
2024-02-26 13:41:20 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+
+import{FormsModule , ReactiveFormsModule} from "@angular/forms";
+import { LogoComponent } from './logo/logo.component';
+import { RegisterComponent } from './register/register.component';
+import { TrainComponent } from './train/train.component';
+import { PhysiqueComponent } from './physique/physique.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    ServiceComponent,
+    ContactComponent,
+    LoginComponent,
+    LogoComponent,
+    RegisterComponent,
+    TrainComponent,
+    PhysiqueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
